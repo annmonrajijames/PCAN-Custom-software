a6b3d702d8586a8fe1aeaf6f4625e17433bec2ee
Annmon James <annmonjameschiramel@gmail.com>
2025-02-05 19:06:03 +0530

    User can enter in binary values so it will send through CAN lines

--- PCAN-Custom-software.py
+++ PCAN-Custom-software.py
@@ -1,21 +1,28 @@
 import can
 
 def send_can_message():
-    # Initialize the CAN bus using the 'pcan' interface.
     try:
         bus = can.interface.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=500000)
     except Exception as e:
         print("Failed to initialize CAN bus:", e)
         return
 
-    # Create a CAN message.
+    # Using binary literals (each value is an integer)
     message = can.Message(
-        arbitration_id=0x00000008,         # The CAN ID
-        data=[0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88],       # Up to 8 data bytes
-        is_extended_id=False          # Use 'is_extended_id' instead of 'extended_id'
+        arbitration_id=0b0000000000001000,  # Same as 0x00000008
+        data=[
+            0b00010001,  # Same as 0x11
+            0b00100010,  # Same as 0x22
+            0b00110011,  # Same as 0x33
+            0b01000100,  # Same as 0x44
+            0b01010101,  # Same as 0x55
+            0b01100110,  # Same as 0x66
+            0b01110111,  # Same as 0x77
+            0b10001000   # Same as 0x88
+        ],
+        is_extended_id=False
     )
 
-    # Attempt to send the message.
     try:
         bus.send(message)
         print("Message sent on", bus.channel_info)
